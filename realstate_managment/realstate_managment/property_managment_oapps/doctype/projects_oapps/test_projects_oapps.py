718b2ad6200e19413e0f92717968857960794ee2
mazhar2101995 <53858324+mazhar2101995@users.noreply.github.com>
2023-01-28 12:32:38 +0530

    Add files via upload

---
+++ realstate_managment/realstate_managment/property_managment_oapps/doctype/projects_oapps/test_projects_oapps.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, inceptionSystem and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestProjectsOapps(FrappeTestCase):
+	pass
